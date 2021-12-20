932d24e8a006d2d6c9c38b97c95279b6483d4e0d
TeerapatEarth <earth_scorpion@hotmail.com>
2021-12-20 21:55:51 +0700

    test deploy

--- src/db/database.js
+++ src/db/database.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose');
-const { MONGO_URI } = process.env;
+const mongoose = require("mongoose");
+const MONGO_URI =
+  "mongodb+srv://teerapat:Earth241043+@cluster0.fa0u1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 exports.connect = () => {
-    mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(() => {
-        console.log("Successfully connected to database");
-    }).catch((error) => {
-        console.log("Error connecting to database123");
-        console.error(error);
-        process.exit(1);
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-}
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("Error connecting to database123");
+      console.error(error);
+      process.exit(1);
+    });
+};
