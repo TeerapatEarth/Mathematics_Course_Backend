f07f96b6b9b79f258e26f5f3b05aebf385ad1766
TeerapatEarth <earth_scorpion@hotmail.com>
2021-12-20 22:16:11 +0700

    CRUD course

--- src/controller/CourseController.js
+++ src/controller/CourseController.js
@@ -1,12 +1,57 @@
 const Course = require("../model/Course")
 
 const CourseController = {
+    createCourse: async function(req, res, next){
+        try {
+            const { title, description, password } = req.body
+            const course = await Course.create({
+                title,
+                description,
+                password
+            })
+            res.status(200).json(course)
+        } catch (err){
+            console.log(err)
+        }
+    },
     getAllCourse: async function(req, res, next){
         try{
             const course = await Course.find({})
             res.status(200).json(course)
         } catch (err){
-            res.status(404).json("Not found")
+            res.status(404).json("Not found.")
+        }
+    },
+    getOneCourse: async function(req, res, next){
+        try{
+            const { id } = req.params
+            const course = await Course.findById(id)
+            res.status(200).json(course)
+        } catch (err){
+            res.status(404).json("Not found course.")
+        }
+    },
+    updateCourse: async function(req, res, next){
+        try {
+            const { id } = req.params
+            const { title, description, password } = req.body
+            await Course.findByIdAndUpdate(id, {
+                title: title,
+                description: description,
+                password: password
+            })
+            res.status(200).json("Update Course")
+        } catch (err){
+            res.status(400).json("Invalid Course ID")
+        }
+    },
+    deleteCourse: async function(req, res, next){
+        try {
+            const { id } = req.body
+            await Course.findByIdAndRemove(id)
+            res.status(200).json("Delete Course")
+        } catch (err){
+            res.status(400).json("Invalid Course ID")
         }
     }
 }
--- src/routes/CourseRoutes.js
+++ src/routes/CourseRoutes.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const CourseController = require("../controller/CourseController")
+const middleware = require("../middleware/CourseMiddleware")
 
 router.get("/course", CourseController.getAllCourse)
+router.get("/course/:id", CourseController.getOneCourse)
+router.post("/course/create", middleware.single("image"), CourseController.createCourse)
+router.put("/course/update/:id", middleware.single("image"), CourseController.updateCourse)
+router.delete("/course/delete/:id", CourseController.deleteCourse)
 
 module.exports = router
