1c3663fc4cace94a0f00581dddd3378fc8075d6a
TeerapatEarth <earth_scorpion@hotmail.com>
2021-12-22 21:55:39 +0700

    update cors

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 require("./src/db/database").connect();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+);
 
-const CourseRoutes = require("./src/routes/CourseRoutes")
-app.use(CourseRoutes)
+const CourseRoutes = require("./src/routes/CourseRoutes");
+app.use(CourseRoutes);
 
 module.exports = app;
