4c72ca00a1411b3cc83057a1a31a7319fac361bf
TeerapatEarth <earth_scorpion@hotmail.com>
2021-12-25 16:19:21 +0700

    fix

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "https://frontend-mathmetics.herokuapp.com/",
-      "http://localhost:3000/",
-    ],
+    origin: "*",
     credentials: true,
+    optionSuccessStatus: 200,
   })
 );
 
